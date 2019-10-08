4865b255e0d19325693b7e8efba99fb601856446
Devon <=>
2019-10-08 11:15:23 -0700

    db connection, mysql overivew

---
+++ 2Week/DBTimes/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DBTimes.Models;
+
+namespace DBTimes.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            var ninjas = DbConnector.Query("SELECT * FROM ninjas");
+            ViewBag.Ninjas = ninjas;
+            return View();
+        }
+        [HttpGet("new")]
+        public IActionResult New()
+        {
+            return View();
+        }
+        [HttpGet("ninja/{ninjaId}")]
+        public IActionResult Ninja(int ninjaId)
+        {
+            string q = $"SELECT * FROM ninjas WHERE NinjaId = {ninjaId}";
+            var result = DbConnector.Query(q);
+            if(result.Count < 1)
+            {
+                return RedirectToAction("Index");
+            }
+            ViewBag.Ninja = result[0];
+            return View();
+        }
+        [HttpGet("ninja/{ninjaId}/delete")]
+        public IActionResult Delete(int ninjaId)
+        {
+            string q = $"DELETE FROM ninjas WHERE NinjaId = {ninjaId}";
+            DbConnector.Execute(q);
+            return RedirectToAction("Index");
+        }
+        [HttpPost("create")]
+        public IActionResult Create(Ninja ninja)
+        {
+            if(ModelState.IsValid)
+            {
+                // create a ninja in the db!
+                string q = $@"
+                    INSERT INTO ninjas (Name, Color, Email, SecretCode, Bio) 
+                    VALUES ('{ninja.Name}', '{ninja.Color}', '{ninja.Email}', '{ninja.SecretCode}', '{ninja.Bio}')
+                ";
+                DbConnector.Execute(q);
+                return RedirectToAction("Index");
+            }
+            return View("New");
+        }
+    }
+}
---
+++ 2Week/DBTimes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DBTimes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
